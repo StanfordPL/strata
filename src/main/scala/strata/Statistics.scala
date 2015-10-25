99e0e7014207f48034bcb4a285ec45e870bb9b82
Stefan Heule <stefanheule@gmail.com>
2015-10-24 23:29:39 -0700

    work in progress

--- src/main/scala/strata/Statistics.scala
+++ src/main/scala/strata/Statistics.scala
@@ -260,9 +260,9 @@ object Statistics {
     val headerLines = header.split("\n")
     val headerWidth = headerLines(0).length
     for ((line, i) <- headerLines.zipWithIndex) {
-      print(" " * 12)
+      print(" " * 4)
       print(line)
-      if (i == headerLines.length - 1) print("   strata v1.0")
+      if (i == headerLines.length - 1) print(s"   strata [git hash ${IO.getGitHash.substring(0, 12)}]")
       println("")
     }
 
