3136b1ca4e33a8b6c1c2066abe1262f8a25631ab
Stefan Heule <stefanheule@gmail.com>
2016-02-12 12:03:50 -0800

    work in progress

---
+++ scripts/graphs/progress.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+
+Plot the progress over time
+
+"""
+import csv
+import config
+import matplotlib.pyplot as plt
+
+config.setup()
+
+# load data
+x = []
+y = []
+with open(config.data_path + '/progress.csv', 'rb') as csvfile:
+  spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+  for row in spamreader:
+    x.append(float(row[0]))
+    y.append(int(float(row[1])))
+
+fig, ax = plt.subplots()
+rects = ax.plot(x, y, '-', linewidth=1)
+
+plt.gca().set_xlim([min(x),max(x)])
+
+plt.xlabel('Wall-clock time elapsed [hours]')
+plt.ylabel('Number of formulas learned')
+
+plt.subplots_adjust(left=0.13, bottom=0.18)
+plt.savefig(config.output + '/progress.pdf')
